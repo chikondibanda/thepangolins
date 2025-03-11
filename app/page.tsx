eceb1c8f2cf8a3f7a8a7ff692548ec558ac60ef0
chikondibanda <chicokbanda@gmail.com>
2025-03-11 09:24:08 +0200

    add arigato

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <h1 className="text-center mt-5">Welcome to The Pangolins!</h1>
+      <p>Arigato</p>
     </div>
   );
 }
